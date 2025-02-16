2648b45f5c4590d7e0fbf3ffba864f3b952c70a4
Virachai Wongsena <wvirachai@gmail.com>
2025-02-16 09:32:22 +0700

    remove: If no session exists, redirect to the home page

--- app/(root)/(homepage)/post/page.tsx
+++ app/(root)/(homepage)/post/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 import PostSection from "@/app/components/Post/PostSection";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/lib/authOptions";
-import { redirect } from "next/navigation";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/app/lib/authOptions";
+// import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Our Blog",
@@ -10,12 +10,12 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   // Get the session from NextAuth
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  // If no session exists, redirect to the home page
-  if (!session) {
-    redirect("/");
-  }
+  // // If no session exists, redirect to the home page
+  // if (!session) {
+  //   redirect("/");
+  // }
 
   return (
     <div>
