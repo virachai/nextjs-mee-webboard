012c20984716e2d57253d3a3d4380632028da237
Virachai Wongsena <wvirachai@gmail.com>
2025-02-17 13:56:45 +0700

    update Our Blog

--- app/(root)/(homepage)/post/page.tsx
+++ app/(root)/(homepage)/post/page.tsx
@@ -1,22 +1,11 @@
 import { Metadata } from "next";
 import PostSection from "@/app/components/Post/PostSection";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/app/lib/authOptions";
-// import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Our Blog",
 };
 
 export default async function Page() {
-  // Get the session from NextAuth
-  // const session = await getServerSession(authOptions);
-
-  // // If no session exists, redirect to the home page
-  // if (!session) {
-  //   redirect("/");
-  // }
-
   return (
     <div>
       <PostSection />
